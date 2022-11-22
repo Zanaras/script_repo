1b8b8cf8533ca3084a792dedceefeef22ce62ba3
Andrew <Zanaras@users.noreply.github.com>
2022-11-22 17:43:05 -0600

    Update install.sh

--- factorio/install.sh
+++ factorio/install.sh
@@ -6,6 +6,7 @@ mkdir /opt/factorio
 mkdir /opt/fup
 wget https://factorio.com/get-download/stable/headless/linux64
 tar -xzf linux64 -C /opt/factorio
+wget -O https://raw.githubusercontent.com/Zanaras/script_repo/main/factorio/server_settings.json /opt/data/server_settings.json
 wget -O https://raw.githubusercontent.com/narc0tiq/factorio-updater/master/update_factorio.py /opt/fup/update_factorio.py
 pip install requests
 mkdir /opt/factorio/mods
