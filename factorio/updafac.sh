6d25cf0d5bffd7414246a353ca8dbe7ad47f1537
Andrew <Zanaras@users.noreply.github.com>
2023-08-31 22:05:13 -0500

    Create updafac.sh

---
+++ factorio/updafac.sh
@@ -0,0 +1,2 @@
+python3 /opt/fup/update_factorio.py -Da /opt/factorio/bin/x64/factorio
+python3 /opt/fmu/mod_updater.py -s /opt/factorio/data/server-settings.json -m /opt/factorio/mods --fact-path /opt/factorio/bin/x64/factorio --update
